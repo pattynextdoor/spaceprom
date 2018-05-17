865771590b9892da35e585c60eb74bb4e37f401d
ptumb001 <ptumb001@ucr.edu>
2018-05-17 00:46:43 -0700

    Fixed project structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
